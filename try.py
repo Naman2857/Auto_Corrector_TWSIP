cb51d3c23b0b4c765df94848ab2ec90ff99d62c8
Naman2857 <naman.aggarwal2857@gmail.com>
2023-09-01 16:56:36 +0530

    first commit

---
+++ try.py
@@ -0,0 +1,104 @@
+import streamlit as st
+from pyresparser import ResumeParser
+import nltk
+nltk.download('stopwords')
+
+def run():
+    st.title("Smart Resume Analyser")
+    st.sidebar.markdown("# Choose User")
+    activities = ["Normal User"]
+    choice = st.sidebar.selectbox("Choose among the given options:", activities)
+
+    if choice == 'Normal User':
+        pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+        if pdf_file is not None:
+            save_image_path = './Uploaded_Resumes/' + pdf_file.name
+            with open(save_image_path, "wb") as f:
+                f.write(pdf_file.getbuffer())
+            resume_data = ResumeParser(save_image_path).get_extracted_data()
+            if resume_data:
+                st.header("*Resume Analysis*")
+                st.success("Hello " + resume_data['name'])
+                st.subheader("*Your Basic info*")
+                try:
+                    st.text('Name: ' + resume_data['name'])
+                    st.text('Email: ' + resume_data['email'])
+                    st.text('Contact: ' + resume_data['mobile_number'])
+                    st.text('Resume pages: ' + str(resume_data['no_of_pages']))
+                except:
+                    pass
+
+                cand_level = ''
+                if resume_data['no_of_pages'] == 1:
+                    cand_level = "Fresher"
+                    st.markdown('''<h4 style='text-align: left; color: #d73b5c;'>You are looking Fresher.</h4>''',
+                                unsafe_allow_html=True)
+                elif resume_data['no_of_pages'] == 2:
+                    cand_level = "Intermediate"
+                    st.markdown('''<h4 style='text-align: left; color: #1ed760;'>You are at intermediate level!</h4>''',
+                                unsafe_allow_html=True)
+                elif resume_data['no_of_pages'] >= 3:
+                    cand_level = "Experienced"
+                    st.markdown('''<h4 style='text-align: left; color: #fba171;'>You are at experience level!''',
+                                unsafe_allow_html=True)
+
+                st.subheader("**Skills Recommendation💡**")
+                keywords = st_tags(label='### Skills that you have',
+                                   text='See our skills recommendation',
+                                   value=resume_data['skills'], key='1')
+
+                ds_keyword = ['tensorflow', 'keras', 'pytorch', 'machine learning', 'deep Learning', 'flask',
+                              'streamlit']
+                web_keyword = ['react', 'django', 'node jS', 'react js', 'php', 'laravel', 'magento', 'wordpress',
+                               'javascript', 'angular js', 'c#', 'flask']
+
+                recommended_skills = []
+                reco_field = ''
+
+                for i in resume_data['skills']:
+                    if i.lower() in ds_keyword:
+                        reco_field = 'Data Science'
+                        st.success("** Our analysis says you are looking for Data Science Jobs.**")
+                        recommended_skills = ['Data Visualization', 'Predictive Analysis', 'Statistical Modeling',
+                                              'Data Mining', 'Clustering & Classification', 'Data Analytics',
+                                              'Quantitative Analysis', 'Web Scraping', 'ML Algorithms', 'Keras',
+                                              'Pytorch', 'Probability', 'Scikit-learn', 'Tensorflow', "Flask",
+                                              'Streamlit']
+                        st.markdown(
+                            '''<h4 style='text-align: left; color: #1ed760;'>Adding this skills to resume will boost🚀 the chances of getting a Job💼</h4>''',
+                            unsafe_allow_html=True)
+                        break
+
+                    elif i.lower() in web_keyword:
+                        reco_field = 'Web Development'
+                        st.success("** Our analysis says you are looking for Web Development Jobs **")
+                        recommended_skills = ['React', 'Django', 'Node JS', 'React JS', 'php', 'laravel', 'Magento',
+                                              'wordpress', 'Javascript', 'Angular JS', 'c#', 'Flask', 'SDK']
+                        st.markdown(
+                            '''<h4 style='text-align: left; color: #1ed760;'>Adding this skills to resume will boost🚀 the chances of getting a Job💼</h4>''',
+                            unsafe_allow_html=True)
+                        break
+
+                st.subheader("**Resume Score📝**")
+                st.markdown(
+                    """
+                    <style>
+                        .stProgress > div > div > div > div {
+                            background-color: #d73b5c;
+                        }
+                    </style>""",
+                    unsafe_allow_html=True,
+                )
+                my_bar = st.progress(0)
+                resume_score = 0
+
+                for percent_complete in range(resume_score):
+                    resume_score += 1
+                    time.sleep(0.1)
+                    my_bar.progress(percent_complete + 1)
+                st.success('** Your Resume Writing Score: ' + str(resume_score) + '**')
+                st.warning(
+                    "** Note: This score is calculated based on the content that you have added in your Resume. **")
+                st.balloons()
+
+run()
